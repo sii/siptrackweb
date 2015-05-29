53de9b0e697db3d8b74f2feeff63de4d07325d6c
Simon Ekstrand <simon@ipeer.se>
2015-05-29 10:27:34 +0200

    Include linked categories in device display.

--- siptrackweb/views/device/utils.py
+++ siptrackweb/views/device/utils.py
@@ -2,19 +2,23 @@ from siptrackweb.views import helpers
 
 def make_device_association_list(device):
     ret = []
-    for assoc in device.listAssociations(include = ['device']):
-        path = helpers.make_browsable_path(assoc,
-                ['device category', 'device tree'],
-                include_root = False)
+    for assoc in device.listAssociations(include = ['device', 'device category']):
+        if assoc.class_name == 'device category':
+            path = [{'path': '/display/%s/' % assoc.oid, 'name': assoc.attributes.get('name', '[device category]')}]
+        else:
+            path = helpers.make_browsable_path(assoc,
+                                               ['device category', 'device tree'],
+                                               include_root = False)
         ent = {'obj': assoc, 'path': path, 'type': 'association'}
         ret.append(ent)
-    for ref in device.listReferences(include = ['device']):
-        path = helpers.make_browsable_path(ref,
-                ['device category', 'device tree'],
-                include_root = False)
+    for ref in device.listReferences(include = ['device', 'device category']):
+        if ref.class_name == 'device category':
+            path = [{'path': '/display/%s/' % ref.oid, 'name': ref.attributes.get('name', '[device category]')}]
+        else:
+            path = helpers.make_browsable_path(ref,
+                    ['device category', 'device tree'],
+                    include_root = False)
         ent = {'obj': ref, 'path': path, 'type': 'reference'}
         ret.append(ent)
     ret.sort()
     return ret
-
-
