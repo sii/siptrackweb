e4d2882595a40d3dbb1bb74c1838937988726d8e
Stefan Midjich <swehack@gmail.com>
2018-11-20 22:23:54 +0100

    added created date to search results.

--- siptrackweb/templatetags/myfilters.py
+++ siptrackweb/templatetags/myfilters.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django import template
 from django.forms.fields import CheckboxInput
 
@@ -6,3 +8,11 @@ register = template.Library()
 @register.filter(name='is_checkbox')
 def is_checkbox(value):
     return isinstance(value, CheckboxInput)
+
+
+DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
+
+@register.filter('date_from_timestamp')
+def date_from_timestamp(timestamp):
+    return datetime.fromtimestamp(int(timestamp))
+
